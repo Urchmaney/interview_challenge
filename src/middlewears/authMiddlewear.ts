69e6aa893a1b8bff47464f8f53fe7e6b4d799430
kingsley-people-box <kingsley@peoplebox.ai>
2023-04-04 01:57:32 +0100

    implement token authentication middle wear

---
+++ src/middlewears/authMiddlewear.ts
@@ -0,0 +1,21 @@
+import { NextFunction, Request, Response } from "express";
+import { AuthRequest } from '../models/custom';
+import { decodeAuthToken } from "../services/tokenService";
+
+
+
+export function tokenAuthMiddlewear (req: Request, res: Response, next: NextFunction) : void {
+  if(!req.headers.authorization) {
+    res.status(401).send("Unauthorized Request");
+    return;
+  }
+  const token = req.headers.authorization?.split(' ')[1];
+  const decoded = decodeAuthToken(token);
+  if(!decoded) {
+    res.status(401).send("Unauthorized Request");
+    return;
+  }
+
+  (req as AuthRequest).user_id = decoded.id;
+  next()
+}
