1de018feb06314d613f07a937314b43f195560d4
kingsley-people-box <kingsley@peoplebox.ai>
2023-04-04 02:08:36 +0100

    map routers to url

--- src/index.ts
+++ src/index.ts
@@ -1,12 +1,19 @@
-import express from 'express';
+import express, { Request, Response, NextFunction } from 'express';
+import { authRouter } from './routes/auth';
+import { transactionRouter } from './routes/transaction';
+
+const bodyParser = require('body-parser')
+
  
 const app: express.Application = express();
- 
+
+app.use(bodyParser.json());
+
 const port: number = 3000;
- 
-app.get('/', (_req, _res) => {
-    _res.send("TypeScript With Express");
-});
+
+app.use('/api/auth', authRouter);
+
+app.use('/api/transactions', transactionRouter);
  
 app.listen(port, () => {
     console.log(`TypeScript with Express
