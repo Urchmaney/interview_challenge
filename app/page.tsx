02360f150fe852b57800867391e0d4502deec70e
kingsley-people-box <kingsley@peoplebox.ai>
2023-09-10 03:29:50 +0100

    hero section

--- app/page.tsx
+++ app/page.tsx
@@ -4,8 +4,8 @@ export default function Home() {
   return (
     <main className='bg-[#fffaf2] px-2.5 '>
       {/* Hero Section */}
-      <div>
-        <div className='w-[95%] md:w-[90%] mx-auto'>
+      <div className='relative'>
+        <div className='w-[95%] md:w-[90%] mx-auto h-screen'>
           <header className="flex flex-wrap sm:justify-start sm:flex-nowrap z-50 w-full bg-[#fffaf2] text-sm py-4 dark:bg-gray-800">
             <nav className="max-w-[85rem] w-full mx-auto px-4 sm:flex sm:items-center sm:justify-between" aria-label="Global">
               <div className="flex items-center justify-between">
@@ -35,12 +35,42 @@ export default function Home() {
                   <a className="font-medium text-blue-500" href="#" aria-current="page">About</a>
                   <a className="font-medium text-gray-600 hover:text-gray-400 dark:text-gray-400 dark:hover:text-gray-500" href="#">Menu</a>
                   <a className="font-medium text-gray-600 hover:text-gray-400 dark:text-gray-400 dark:hover:text-gray-500" href="#">Support</a>
-                  <button className="py-3 px-4 inline-flex justify-center items-center gap-2 rounded-full border border-transparent font-semibold bg-blue-500 text-white hover:bg-blue-600 focus:outline-none focus:ring-2 focus:ring-blue-500 focus:ring-offset-2 transition-all text-sm dark:focus:ring-offset-gray-800">Get Started</button>
+                  <button className='py-3 px-4 inline-flex justify-center items-center gap-2 rounded-full border border-transparent font-semibold bg-[#f18318] hover:bg-[#c8560f] text-white transition-all text-sm'>Get Started</button>
                 </div>
-                
+
               </div>
             </nav>
           </header>
+          <div className='mt-[130px]'>
+            <h1 className='text-[#f18318] text-[65px] font-bold'>
+              No <span className='text-[#241a43]'>Canteen?</span> <br />
+              No Problem
+            </h1>
+            <p className='text-[20px] font-normal leading-7 mt-[20px] mb-[30px]'>
+              Stay fuelled at work with our digital cafeteria.
+              Great food everyday, right at lunch time!
+            </p>
+            <div className='flex gap-3'>
+              <button className='py-3 px-4 inline-flex justify-center items-center gap-2 rounded-full border border-transparent font-semibold bg-[#f18318] hover:bg-[#c8560f] text-white transition-all text-sm'>
+                Get Started
+              </button>
+              <button className='py-3 px-4 inline-flex justify-center items-center gap-2 rounded-full border border-[#ffe4cc] hover:border-[#ffc188] font-semibold bg-[#fff4ea] text-black hover:bg-[#ffdebf] transition-all text-sm '>
+                Download App
+              </button>
+            </div>
+            <div className='static md:absolute right-0 -bottom-28 w-[100%] md:w-[45%] mt-[30px]'>
+              <img src='/images/imagehero.webp'  alt="Hero Food" className="w-[100%] h-[100%]"  />
+              {/* <Image
+                src="/images/imagehero.webp"
+                alt="Hero Food"
+                className="w-[100%] h-[100%]"
+                width={'100'}
+                height={24}
+                priority
+              /> */}
+            </div>
+
+          </div>
         </div>
 
       </div>
